fc8132e680c92c618f697ca1d3f6b81d7a63a2aa
Erivan Serafim <erivan_serafim_fernandes@hotmail.com>
2026-02-15 22:34:32 -0300

    incluindo serviços FTP

---
+++ src/monitoramento/ftp_services.py
@@ -0,0 +1,55 @@
+from ftplib import FTP
+from pathlib import Path
+from logger import logger
+
+
+def connect_ftp(host, user, pwd) -> FTP:
+    """
+    Conecta ao servidor FTP da IHM e retorna o objeto FTP.
+    """
+    try:
+        ftp = FTP()
+        ftp.connect(host, 21, timeout=10)
+        ftp.login(user, pwd)
+        logger.info("Conexão FTP feita com sucesso!")
+        return ftp
+    except Exception as e:
+        logger.error("Erro ao estabelecer a conexão FTP: %s", e)
+        return None
+
+
+def list_files(ftp: FTP, directory: str, show_info: bool = False) -> list:
+    """
+    Lista arquivos em um diretório da IHM.
+    """
+    try:
+        logger.info(f"\nListando arquivos em: {directory}")
+        ftp.cwd(directory)
+        files = ftp.nlst()
+        if show_info:
+            for f in files:
+                logger.info(f" - {f}")
+        return files
+    except Exception as e:
+        logger.error("Erro ao listar arquivos: %s", e)
+        return []
+
+
+def download_file(ftp: FTP,
+                  remote_dir: str,
+                  remote_filename: str,
+                  local_path: Path) -> bool:
+    """
+    Baixa um arquivo do FTP (IHM) para o PC.
+    """
+    try:
+        logger.info(
+            f"Baixando '{remote_filename}' de '{remote_dir}' para '{local_path}' ...")
+        ftp.cwd(remote_dir)
+        with open(local_path, "wb") as f:
+            ftp.retrbinary(f"RETR {remote_filename}", f.write)
+        logger.info(f"Download concluído!")
+        return True
+    except Exception as e:
+        logger.error("Erro fazer download do arquivo: %s", e)
+        return False
